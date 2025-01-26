7ba0d0026b1fe6d0c6939691ef3c2fb465a6276b
Jonatan Branting <jonatan.branting@gmail.com>
2025-01-26 23:10:18 +0100

    Add stylua (#20)
    * Add stylua.toml
    
    * Run stylua

--- lua/better-n/init.lua
+++ lua/better-n/init.lua
@@ -4,60 +4,60 @@ local Config = require("better-n.config")
 local M = {}
 
 function M.instance()
-	if _G.better_n_register ~= nil then
-		return _G.better_n_register
-	end
+  if _G.better_n_register ~= nil then
+    return _G.better_n_register
+  end
 
-	_G.better_n_register = Register:new()
+  _G.better_n_register = Register:new()
 
-	return _G.better_n_register
+  return _G.better_n_register
 end
 
 function M.setup(opts)
-	if opts.mappings then
-		vim.deprecate(
-			"opts.mappings is deprecated",
-			'create mappings manually using `require("better-n").create({ next = ..., previous = ... })`',
-			"HEAD",
-			"nvim-better-n",
-			false
-		)
-	end
-	if opts.callbacks then
-		vim.deprecate(
-			"opts.callbacks",
-			"Use `vim.api.nvim_create_autocmd` to listen to the User event with pattern `BetterNMappingExecuted` instead",
-			"HEAD",
-			"nvim-better-n",
-			false
-		)
-	end
-	local defaults = Config.get_default_config()
-	local config = vim.tbl_deep_extend("force", defaults, opts)
+  if opts.mappings then
+    vim.deprecate(
+      "opts.mappings is deprecated",
+      'create mappings manually using `require("better-n").create({ next = ..., previous = ... })`',
+      "HEAD",
+      "nvim-better-n",
+      false
+    )
+  end
+  if opts.callbacks then
+    vim.deprecate(
+      "opts.callbacks",
+      "Use `vim.api.nvim_create_autocmd` to listen to the User event with pattern `BetterNMappingExecuted` instead",
+      "HEAD",
+      "nvim-better-n",
+      false
+    )
+  end
+  local defaults = Config.get_default_config()
+  local config = vim.tbl_deep_extend("force", defaults, opts)
 
-	Config.apply_config(config)
+  Config.apply_config(config)
 
-	return M
+  return M
 end
 
 function M.next()
-	return M.instance():next()
+  return M.instance():next()
 end
 
 function M.previous()
-	return M.instance():previous()
+  return M.instance():previous()
 end
 
 function M.n()
-	return M.next()
+  return M.next()
 end
 
 function M.shift_n()
-	return M.previous()
+  return M.previous()
 end
 
 function M.create(...)
-	return M.instance():create(...)
+  return M.instance():create(...)
 end
 
 return M
