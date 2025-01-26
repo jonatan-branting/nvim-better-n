f8834dd60042d2d53d19a4841f92e71c90176e39
Jonatan Branting <jonatan.branting@gmail.com>
2025-01-26 22:57:00 +0100

    Use default test folder

--- spec/init.lua
+++ spec/init.lua
@@ -4,6 +4,6 @@ vim.opt.swapfile = false
 
 vim.api.nvim_command([[set rtp+=.]])
 vim.api.nvim_command(
-  string.format([[set rtp+=%s/lua_modules/lib/lua/5.1/,%s/tests/]], cwd, cwd)
+  string.format([[set rtp+=%s/lua_modules/lib/lua/5.1/,%s/spec/]], cwd, cwd)
 )
 vim.api.nvim_command([[packloadall]])
