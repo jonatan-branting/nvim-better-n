c90c2c75fe023813d5592a6ed80c6e4e5b870689
Jonatan Branting <jonatan.branting@gmail.com>
2025-04-01 19:52:36 +0200

    Add Keymap class

---
+++ lua/better-n/keymap.lua
@@ -0,0 +1,38 @@
+local Enumerable = require("better-n.enumerable")
+
+local Keymap = {}
+
+function Keymap:new(opts)
+  local instance = {
+    bufnr = opts.bufnr,
+    mode = opts.mode or "n",
+  }
+
+  local raw_keymap = instance.bufnr and
+    vim.api.nvim_buf_get_keymap(instance.bufnr, instance.mode) or
+    vim.api.nvim_get_keymap(instance.mode)
+
+  instance.keymap = Enumerable:new(raw_keymap)
+  setmetatable(instance, Keymap)
+
+  return instance
+end
+
+Keymap.__index = function(self, key)
+  -- Support accessing instance variables directly
+  local class_value = rawget(self, key)
+  if class_value ~= nil then
+    return class_value
+  end
+
+  -- Ensure it has the expected casing and format
+  local transformed_key = vim.fn.keytrans(vim.keycode(key))
+  local keymap = rawget(self, "keymap")
+
+  return keymap:find(function(mapping)
+    return mapping.lhs == transformed_key
+  end)
+end
+
+return Keymap
+
